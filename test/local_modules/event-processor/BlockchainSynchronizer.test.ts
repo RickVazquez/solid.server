7f51e26de857dab12cb39826f024800a631b8b38
Rick vazquez <rick_vazquez@outlook.com>
2019-10-12 04:17:12 +0900

    tests

--- test/local_modules/event-processor/BlockchainSynchronizer.test.ts
+++ test/local_modules/event-processor/BlockchainSynchronizer.test.ts
@@ -1,52 +1,163 @@
 import AsyncPolling from 'async-polling'
-
-import { Connection, buildFakeConnection, buildFakeBlock, buildFakeBlocks, buildFakeTransactions } from '@solidstudio/solid.types'
+import { Connection, buildFakeConnection, buildFakeBlock, buildFakeBlocks, buildFakeTransactions, buildFakeTransaction, buildFakeTransactionReceipt } from '@solidstudio/solid.types'
+import { BlockchainSynchronizer } from '../../../src/local_modules/event-processor/BlockchainSynchronizer'
+import { PollingService } from '../../../src/local_modules/event-processor/PollingService';
+import { IWeb3Wrapper } from '../../../src/local_modules/web3-wrapper/IWeb3Wrapper'
+import { Web3Wrapper } from '../../../src/local_modules/web3-wrapper/Web3Wrapper'
+import { randomHex } from 'web3-utils'
 
 import app from '../../../src/app';
 
-import { BlockchainSynchronizer } from '../../../src/local_modules/event-processor/BlockchainSynchronizer'
-import { PollingService } from '../../../src/local_modules/event-processor/PollingService';
+jest.mock('../../../src/local_modules/web3-wrapper/Web3Wrapper')
+
+const delay = (ms: number) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+jest.setTimeout(10000000)
 
 describe('BlockchainSynchronizer tests', () => {
 
-    let blockchainSynchronizer: BlockchainSynchronizer
-    let sampleConnection: Connection
-    let pollingFactory: PollingService
+    // TODO: Get code.
+    beforeAll(async () => {
+        const startTime: number = Date.now()
 
-    it.skip('should help me create a strong poller mechanism with transaction guarantee', async () => {
-        sampleConnection = buildFakeConnection()
-        pollingFactory = new PollingService()
-        blockchainSynchronizer = new BlockchainSynchronizer(sampleConnection, pollingFactory, app)
+        const sequelise = app.get('sequelizeClient')
 
-        blockchainSynchronizer.start()
+        await sequelise.sync({ force: true });
+
+        (Web3Wrapper as any).mockImplementation(() => {
+            return {
+                getBlockNumber: () => {
+                    console.log("Calling getBlockNumber", Date.now() - startTime)
+                    return Promise.resolve(2);
+                },
+                getBlock: (blockNumber: number) => {
+                    console.log(`Calling getBlock ${blockNumber}`, Date.now() - startTime)
+                    return Promise.resolve(buildFakeBlock({
+                        blockNumber,
+                        transactions: [randomHex(32), randomHex(32)]
+                    }))
+                },
+                getTransaction: (txHash: string) => {
+                    console.log("Calling getTransaction", Date.now() - startTime)
+                    return Promise.resolve(buildFakeTransaction({
+                        hash: txHash
+                    }))
+                },
+                getTransactionReceipt: (txHash: string) => {
+                    console.log("Calling getTransactionReceipt", Date.now() - startTime)
+                    return Promise.resolve(buildFakeTransactionReceipt({
+                        transactionHash: txHash
+                    }))
+                },
+                getTransactionCount: (contractAddress: string) => {
+                    console.log("Calling getTransactionCount", Date.now() - startTime)
+                    return Promise.resolve(1);
+                },
+                getCode: (contractAddress: string) => {
+                    console.log("Calling getCode", Date.now() - startTime)
+                    return Promise.resolve("pragma solidity 0.4.12;..");
+                },
+                getDefaultAccount: jest.fn()
+            };
+        });
     })
 
-    it.only('should make a transaction', async (done) => {
-        const block = buildFakeBlock()
-        block.transactions = JSON.stringify(block.transactions) as any
-        block.uncles = JSON.stringify(block.uncles) as any
+    it.only('should process a block', async () => {
+        const sampleConnection = buildFakeConnection({
+            name: "TESTxx2"
+        })
 
-        const transactions = buildFakeTransactions()
+        delete sampleConnection.id;
+
+        const connectionsService = app.service('connections')
+        const blocksService = app.service('blocks')
+        const transactionsService = app.service('transactions')
+        const transactionReceiptsService = app.service('transaction-receipts')
+
+        const newConnection = await connectionsService.create(sampleConnection);
+
+        expect(newConnection).toBeDefined()
+        expect(newConnection.id).toBeGreaterThan(0)
+        expect(newConnection.lastBlockNumberProcessed).toEqual(0)
+
+        await delay(10000)
+
+        const connectionAfterDelay = await connectionsService.get(newConnection.id)
+
+        expect(connectionAfterDelay.lastBlockNumberProcessed).toEqual(2)
+
+        // blocks
+        const totalBlocks = await blocksService.find()
+
+        expect(totalBlocks).toBeDefined()
+        expect(totalBlocks.data).toHaveLength(2)
+
+        // transactions
+        const totalTransactions = await transactionsService.find()
+
+        expect(totalTransactions).toBeDefined()
+        expect(totalTransactions.data).toHaveLength(4)
+
+        // transactionReceipts
+        const transactionReceipts = await transactionReceiptsService.find()
+
+        expect(transactionReceipts).toBeDefined()
+        expect(transactionReceipts.data).toHaveLength(4)
+
+        await connectionsService.remove(newConnection.id)
+
+        await delay(10000)
+
+        expect((await connectionsService.find()).data).toHaveLength(0)
+        expect((await blocksService.find()).data).toHaveLength(0)
+        expect((await transactionsService.find()).data).toHaveLength(0)
+        expect((await transactionReceiptsService.find()).data).toHaveLength(0)
+    })
+
+    it.skip('should help me to understand how to make a transaction', async (done) => {
+        const sampleConnection = buildFakeConnection({
+            name: "TESTxx2"
+        })
+
+        delete sampleConnection.id;
 
         const sequelize = app.get('sequelizeClient');
         const transactionsService = app.service('transactions')
         const blocksService = app.service('blocks')
+        const connectionsService = app.service('connections')
+        const newConnection = await connectionsService.create(sampleConnection);
 
-        sequelize.transaction(transaction => {
-            return blocksService.create(block, { sequelize: { transaction } })
-                .then((whatIsThis) => {
-                    console.log("whatIsThis", whatIsThis)
-                    return transactionsService.create(transactions, { sequelize: { transaction } })
-                })
-        }).then(result => {
+        console.log("newConnection", newConnection)
+        const block = buildFakeBlock({
+            connectionId: newConnection.id
+        })
+
+        const transactionValue = buildFakeTransaction({
+            connectionId: newConnection.id
+        })
+
+        sequelize.transaction(async (transaction) => {
+            await blocksService.create(block, { sequelize: { transaction } })
+            await transactionsService.create(transactionValue, { sequelize: { transaction } })
+            // throw new Error('something wrong')
+            return await connectionsService.update(newConnection.id, {
+                ...newConnection,
+                lastBlockNumberProcessed: 10
+            }, { sequelize: { transaction } })
+        }).then(async (result) => {
             // Transaction has been committed
             // result is whatever the result of the promise chain returned to the transaction callback
-            console.log("RESULT", result)
+            console.log("TRANSACTION COMPLETE, RESULT", result)
+            console.log("ABOUT TO REMOVE CONNECTION")
+            await connectionsService.remove(newConnection.id)
             done()
         }).catch(err => {
             // Transaction has been rolled back
             // err is whatever rejected the promise chain returned to the transaction callback
-            console.log("error", err)
+            console.log("TRANSACTION FAILED, error", err)
             done()
         });
     })
@@ -67,7 +178,7 @@ describe('BlockchainSynchronizer tests', () => {
         console.log('RESULT', result)
     })
 
-    it.skip('should help me understand the behaviour of AsyncPolling', (done) => {
+    it.skip('should help me understand the behaviour of AsyncPolling', async (done) => {
 
         let counter = 0;
         // it should call synchronize after 5 ms?
@@ -77,7 +188,7 @@ describe('BlockchainSynchronizer tests', () => {
 
         const otherFunction = () => {
             return new Promise((resolve, reject) => {
-                console.log("I'm inside another function", Date.now() - startTime)
+                console.log("I'm inside another function", Date.now() - startTime, counter)
                 setTimeout(() => {
                     console.log("I'm inside resolved", Date.now() - startTime)
                     resolve(true)
@@ -85,7 +196,7 @@ describe('BlockchainSynchronizer tests', () => {
             })
         }
 
-        AsyncPolling((end) => {
+        const asyncPolling = AsyncPolling((end) => {
             // Do whatever you want.
             // console.timeLog('synchronizer');
             otherFunction()
@@ -95,10 +206,17 @@ describe('BlockchainSynchronizer tests', () => {
                     console.log("Synchronized After END", Date.now() - startTime);
                     counter++
                 })
-            if (counter >= 5) {
+            if (counter >= 500) {
                 done()
                 console.timeEnd('Synchronizer');
             }
-        }, 100).run(); // este valor es dependiendo el block period y ya, lo demas recursivo hasta que se sincronize...
+        }, 100)//.run(); // este valor es dependiendo el block period y ya, lo demas recursivo hasta que se sincronize...
+
+        await asyncPolling.run()
+
+        await delay(10000)
+
+        console.log("ABOUT TO STOP")
+        await asyncPolling.stop()
     })
 })
