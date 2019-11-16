f8ac344628153a9ec811473279d4a120e37843d9
Solid Studio <solidstudio@labchain.io>
2019-11-16 23:56:15 +0000

    removed unused files

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { removeAllContractDefinitionsIfExists, addContractDefinitionsFromFaker } from './utils'
-
-import logger from './logger';
-import app from './app';
+import { app, logger } from '@solid-explorer/server'
 
 const port = app.get('port');
-const server = app.listen(port);
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+export const startServer = () => {
+    const server = app.listen(port);
 
-server.on('listening', async () => {
-  // TODO: REMOVE AFTER DEMO
-  await removeAllContractDefinitionsIfExists(app)
-  await addContractDefinitionsFromFaker(app)
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-});
+    process.on('unhandledRejection', (reason, p) =>
+        logger.error('Unhandled Rejection at: Promise ', p, reason)
+    );
+
+    server.on('listening', async () => {
+        logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+    });
+}
+
+startServer()
--- src/local_modules/event-processor/IPollingService.ts
+++
@@ -1,5 +0,0 @@
-import { IBlockchainProcessor } from './IBlockchainProcessor'
-
-export interface IPollingService {
-  createPolling(pollingFunction: (end) => void, intervalMs: number): IBlockchainProcessor
-}
